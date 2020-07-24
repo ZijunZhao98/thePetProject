d3093618a32dafc6c8414389319322e304dc507e
zijunzhao <z.zhao@wustl.edu>
2020-07-24 21:58:50 +0800

    installed bolt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const store = require('./store');
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN
-});  
+});
 
 
 app.event('app_home_opened', ({ event, say }) => {
