26d19589eedf59b238188b775f6cd24745e66972
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2020-07-27 19:22:20 -0700

    update pet model

---
+++ models/petmodel.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var connect = process.env.MONGODB_URI;
+
+var Schema = mongoose.Schema
+
+mongoose.connect(connect);
+
+var petSchema = new Schema({
+    slack_id: String,
+    pet_name: String,
+    pet_number: String,
+    create_date: Date,
+    health: Number,
+    happiness: Number,
+    level: Number,
+});
+
+var Pet = mongoose.model('Pet', petSchema);
+
+module.exports = {
+    Pet: Pet,
+};
